eda9fa3886a8bad4371fd1c718ee590187bdaf2c
Austin Blain <austin.a.blain@gmail.com>
2017-01-06 11:53:57 -0800

    change how the information is stored in the HTML using JQuery

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,22 +1,24 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctor = function(symptom, result) {
+  $(".answerHere").empty();
   for(j=0; j < result.data.length; j++) {
-    $(".answerHere").append("Dr." + result.data[j].profile.first_name + " " + result.data[j].profile.last_name + " from " + result.data[j].practices[0].name + " could help with your " + symptom + "!");
-    $(".answerHere").append(" You can reach her at ")
+    $(".answerHere").append('<div class="box-' + j + '"></div>')
+    $(".answerHere div:last").append('<div class="picture">' + '<img src="' + result.data[j].profile.image_url + '">' + '</div>');
+    $(".answerHere div:last").append('<div class="name">' + "Dr." + result.data[j].profile.first_name + " " + result.data[j].profile.last_name + " from " + result.data[j].practices[0].name + " could help with your " + symptom + "!" + '</div>');
+    $(".answerHere div:last").append(" Contact: ")
     // if (result.data[j].practices[0].phones !== undefined) {
       for(i=0; i < result.data[j].practices[0].phones.length; i++) {
-        $('.answerHere').append(result.data[j].practices[0].phones[i].type + ":" + result.data[j].practices[0].phones[i].number + " ");
+        $('.answerHere div:last').append(result.data[j].practices[0].phones[i].type + ":" + result.data[j].practices[0].phones[i].number + " ");
       }
+    // $(".answerHere").append("</div>")
     // }
-    $(".answerHere").prepend('<img src="' + result.data[j].profile.image_url + '">');
   }
 };
 
 $(document).ready(function() {
   var currentDoctorObject = new Doctor();
   $('.symptomForm').submit(function(event) {
-    // debugger;
     event.preventDefault();
     var medicalIssue = $('#symptom').val();
     $('#symptom').val();
--- js/doctor.js
+++ js/doctor.js
@@ -6,11 +6,9 @@ function Doctor() {
 
 
 Doctor.prototype.getDoctors = function(medicalIssue, displayFunction) {
-  // debugger;
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
       console.log(result);
-      // console.log(result.data[0].practices[0].name);
       displayFunction(medicalIssue, result);
     })
    .fail(function(error){
