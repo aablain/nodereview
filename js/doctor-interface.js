664dba3c5db778ee114aef7a15826bd9dcfdfc22
Austin Blain <austin.a.blain@gmail.com>
2017-01-06 10:34:20 -0800

    add for function that cycles through all recieved profiles for information in each of them

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,12 +1,16 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctor = function(symptom, result) {
-  $(".answerHere").append("Dr." + result.data[0].profile.first_name + " " + result.data[0].profile.last_name + " from " + result.data[0].practices[0].name + " could help with your " + symptom + "!");
-  $(".answerHere").append(" You can reach her at ")
-  for(i=0; i < result.data[0].practices[0].phones.length; i++) {
-    $('.answerHere').append(result.data[0].practices[0].phones[i].type + ":" + result.data[0].practices[0].phones[i].number + " ");
+  for(j=0; j < result.data.length; j++) {
+    $(".answerHere").append("Dr." + result.data[j].profile.first_name + " " + result.data[j].profile.last_name + " from " + result.data[j].practices[0].name + " could help with your " + symptom + "!");
+    $(".answerHere").append(" You can reach her at ")
+    // if (result.data[j].practices[0].phones !== undefined) {
+      for(i=0; i < result.data[j].practices[0].phones.length; i++) {
+        $('.answerHere').append(result.data[j].practices[0].phones[i].type + ":" + result.data[j].practices[0].phones[i].number + " ");
+      }
+    // }
+    $(".answerHere").prepend('<img src="' + result.data[j].profile.image_url + '">');
   }
-  $(".answerHere").prepend('<img src="' + result.data[0].profile.image_url + '">');
 };
 
 $(document).ready(function() {
