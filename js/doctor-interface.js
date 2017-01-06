8ab6aa5a7fa468c90169f9d27417e29e820a242a
Austin Blain <austin.a.blain@gmail.com>
2017-01-06 08:40:09 -0800

    properly show an image of the doctor along with name and name of practice

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,12 +2,13 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctor = function(symptom, result) {
   $(".answerHere").text("Dr." + result.data[0].profile.first_name + " " + result.data[0].profile.last_name + " from " + result.data[0].practices[0].name + " could help with your " + symptom);
+  $(".answerHere").prepend('<img src="' + result.data[0].profile.image_url + '">')
 };
 
 $(document).ready(function() {
   var currentDoctorObject = new Doctor();
   $('.symptomForm').submit(function(event) {
-    debugger;
+    // debugger;
     event.preventDefault();
     var medicalIssue = $('#symptom').val();
     $('#symptom').val();
